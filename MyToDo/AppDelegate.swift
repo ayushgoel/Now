67b5f797bea4444e999618fe0399daab5a1793ec
Ayush Goel <ayushgoel111@gmail.com>
2016-06-23 16:38:56 +0530

    Initial Commit

---
+++ MyToDo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MyToDo
+//
+//  Created by Ayush Goel on 23/06/16.
+//  Copyright © 2016 Ayush Goel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
